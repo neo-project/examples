67a7a9300ee19c54008ef145e4df43cc722008ad
Erik Zhang <erik@vcage.com>
2017-05-15 16:46:12 +0800

    upgrade to 1.6.0-preview8

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -7,9 +7,7 @@ namespace AntShares.SmartContract
     {
         public static void Main()
         {
-            byte[] key = { 0x48, 0x65, 0x6c, 0x6c, 0x6f }; // ASCII of "Hello"
-            byte[] value = { 0x57, 0x6f, 0x72, 0x6c, 0x64 }; // ASCII of "World"
-            Storage.Put(StorageContext.Current, key, value);
+            Storage.Put(StorageContext.Current, "Hello", "World");
         }
     }
 }
