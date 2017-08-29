cdc71adfe3598d8259351e92330f72352cfaf744
Wing Yung Chan <wy@users.noreply.github.com>
2017-08-29 23:05:59 +0100

    Added Runtime Logging - Issue-1
    Impossible to see return values so Runtime.Log gives this visibility when debugging smart contracts

--- Domain/Domain.cs
+++ Domain/Domain.cs
@@ -1,4 +1,4 @@
-﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework;
 using Neo.SmartContract.Framework.Services.Neo;
 
 namespace Neo.SmartContract
@@ -7,6 +7,8 @@ namespace Neo.SmartContract
     {
         public static object Main(string operation, params object[] args)
         {
+            Runtime.Log("Operation: " + operation);
+
             switch (operation)
             {
                 case "query":
@@ -24,35 +26,78 @@ namespace Neo.SmartContract
 
         private static byte[] Query(string domain)
         {
-            return Storage.Get(Storage.CurrentContext, domain);
+            byte[] value = Storage.Get(Storage.CurrentContext, domain);
+            Runtime.Log("Query: " + domain + " : " + BytesToHex(value));
+            return value;
         }
 
         private static bool Register(string domain, byte[] owner)
         {
-            if (!Runtime.CheckWitness(owner)) return false;
+            if (!Runtime.CheckWitness(owner))
+            {
+                Runtime.Log("Register: Failed Check Witness on Owner");
+                return false;
+            }
             byte[] value = Storage.Get(Storage.CurrentContext, domain);
-            if (value != null) return false;
+            if (value != null)
+            {
+                Runtime.Log("Register: Failed as Owner already exists");
+                return false;
+            }
             Storage.Put(Storage.CurrentContext, domain, owner);
+            Runtime.Log("Register: Successful for domain: " + domain);
             return true;
         }
 
         private static bool Transfer(string domain, byte[] to)
         {
-            if (!Runtime.CheckWitness(to)) return false;
+            if (!Runtime.CheckWitness(to))
+            {
+                Runtime.Log("Transfer: Failed Check Witness on Owner");
+                return false;
+            }
             byte[] from = Storage.Get(Storage.CurrentContext, domain);
-            if (from == null) return false;
-            if (!Runtime.CheckWitness(from)) return false;
+            if (from == null)
+            {
+                Runtime.Log("Transfer: Failed No domain owner");
+                return false;
+            }
+            if (!Runtime.CheckWitness(from))
+            {
+                Runtime.Log("Transfer: Failed Check Witness on From");
+                return false;
+            }
             Storage.Put(Storage.CurrentContext, domain, to);
+            Runtime.Log("Transfer: Succeeded");
             return true;
         }
 
         private static bool Delete(string domain)
         {
             byte[] owner = Storage.Get(Storage.CurrentContext, domain);
-            if (owner == null) return false;
-            if (!Runtime.CheckWitness(owner)) return false;
+            if (owner == null)
+            {
+                Runtime.Log("Delete: Failed no Owner");
+                return false;
+            }
+            if (!Runtime.CheckWitness(owner))
+            {
+                Runtime.Log("Delete: Failed Check Witness on Owner");
+                return false;
+            }
             Storage.Delete(Storage.CurrentContext, domain);
+            Runtime.Log("Delete: Succeeded");
             return true;
+        }
+
+        private static string BytesToHex(byte[] bytes)
+        {
+            string hexString = "";
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                hexString += bytes[i];
+            }
+            return hexString;
         }
     }
 }
