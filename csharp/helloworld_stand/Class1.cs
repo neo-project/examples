6c259bd39f09f4c5fe4ebae2241e819f07901962
lights <lightsever@hotmail.com>
2019-08-08 16:40:57 +0800

    add

---
+++ csharp/helloworld_stand/Class1.cs
@@ -0,0 +1,12 @@
+﻿using Neo.SmartContract.Framework.Services.Neo;
+
+namespace Neo.SmartContract
+{
+    public class HelloWorld : Framework.SmartContract
+    {
+        public static void Main()
+        {
+            Storage.Put(Storage.CurrentContext, "Hello", "World");
+        }
+    }
+}
