66301ee3e53bf545dd53d4f7ffac2b917115ab32
erikzhang <erik@neo.org>
2019-12-10 14:26:40 +0800

    Update NEP-5 template

--- csharp/NEP5/NEP5.cs
+++ csharp/NEP5/NEP5.cs
@@ -9,105 +9,70 @@ namespace NEP5
 {
     public class NEP5 : SmartContract
     {
-        [DisplayName("transfer")]
+        [DisplayName("Transfer")]
         public static event Action<byte[], byte[], BigInteger> Transferred;
 
         private static readonly byte[] Owner = "Ae2d6qj91YL3LVUMkza7WQsaTYjzjHm4z1".ToScriptHash(); //Owner Address
+        private static readonly byte[] callingscript = ExecutionEngine.CallingScriptHash;
 
-        public static object Main(string method, object[] args)
-        {
-            if (Runtime.Trigger == TriggerType.Verification)
-            {
-                return Runtime.CheckWitness(Owner);
-            }
-            else if (Runtime.Trigger == TriggerType.Application)
-            {
-                var callscript = ExecutionEngine.CallingScriptHash;
-
-                if (method == "balanceOf") return BalanceOf((byte[])args[0]);
-
-                if (method == "decimals") return Decimals();
-
-                if (method == "name") return Name();
-
-                if (method == "symbol") return Symbol();
-
-                if (method == "supportedStandards") return SupportedStandards();
-
-                if (method == "totalSupply") return TotalSupply();
-
-                if (method == "transfer") return Transfer((byte[])args[0], (byte[])args[1], (BigInteger)args[2], callscript);
-            }
-            return false;
-        }
-
-        [DisplayName("balanceOf")]
         public static BigInteger BalanceOf(byte[] account)
         {
             if (account.Length != 20)
                 throw new InvalidOperationException("The parameter account SHOULD be 20-byte addresses.");
-            StorageMap asset = Storage.CurrentContext.CreateMap(nameof(asset));
-            return asset.Get(account).AsBigInteger();
+            return Storage.Get(account)?.AsBigInteger() ?? 0;
         }
-        [DisplayName("decimals")]
+
         public static byte Decimals() => 8;
 
-        private static bool IsPayable(byte[] to)
-        {
-            var c = Blockchain.GetContract(to);
-            return c == null || c.IsPayable;
-        }
-
-        [DisplayName("name")]
         public static string Name() => "MyToken"; //name of the token
 
-        [DisplayName("symbol")]
         public static string Symbol() => "MYT"; //symbol of the token
 
-        [DisplayName("supportedStandards")]
-        public static string[] SupportedStandards() => new string[] { "NEP-5", "NEP-7", "NEP-10" };
+        public static string[] SupportedStandards() => new string[] { "NEP-5", "NEP-10" };
 
-        [DisplayName("totalSupply")]
         public static BigInteger TotalSupply()
         {
-            StorageMap contract = Storage.CurrentContext.CreateMap(nameof(contract));
-            return contract.Get("totalSupply").AsBigInteger();
+            return Storage.Get("totalSupply")?.AsBigInteger() ?? 0;
         }
-#if DEBUG
-        [DisplayName("transfer")] //Only for ABI file
-        public static bool Transfer(byte[] from, byte[] to, BigInteger amount) => true;
-#endif
-        //Methods of actual execution
-        private static bool Transfer(byte[] from, byte[] to, BigInteger amount, byte[] callscript)
+
+        public static bool Transfer(byte[] from, byte[] to, BigInteger amount)
         {
+            if (Runtime.Trigger != TriggerType.Application) throw new InvalidOperationException();
+
             //Check parameters
             if (from.Length != 20 || to.Length != 20)
                 throw new InvalidOperationException("The parameters from and to SHOULD be 20-byte addresses.");
-            if (amount <= 0)
+            if (amount < 0)
                 throw new InvalidOperationException("The parameter amount MUST be greater than 0.");
-            if (!IsPayable(to))
+            if (Blockchain.GetContract(to)?.IsPayable == false)
                 return false;
-            if (!Runtime.CheckWitness(from) && from.AsBigInteger() != callscript.AsBigInteger())
+            if (from != callingscript && !Runtime.CheckWitness(from))
                 return false;
-            StorageMap asset = Storage.CurrentContext.CreateMap(nameof(asset));
-            var fromAmount = asset.Get(from).AsBigInteger();
-            if (fromAmount < amount)
-                return false;
-            if (from == to)
-                return true;
 
-            //Reduce payer balances
-            if (fromAmount == amount)
-                asset.Delete(from);
-            else
-                asset.Put(from, fromAmount - amount);
+            StorageContext context = Storage.CurrentContext;
+            var fromAmount = Storage.Get(context, from)?.AsBigInteger() ?? 0;
+            if (fromAmount < amount) return false;
 
-            //Increase the payee balance
-            var toAmount = asset.Get(to).AsBigInteger();
-            asset.Put(to, toAmount + amount);
+            if (from != to && amount > 0)
+            {
+                //Reduce payer balances
+                if (fromAmount == amount)
+                    Storage.Delete(context, from);
+                else
+                    Storage.Put(context, from, fromAmount - amount);
+
+                //Increase the payee balance
+                var toAmount = Storage.Get(context, to)?.AsBigInteger() ?? 0;
+                Storage.Put(context, to, toAmount + amount);
+            }
 
             Transferred(from, to, amount);
             return true;
+        }
+
+        public static bool Verify()
+        {
+            return Runtime.CheckWitness(Owner);
         }
     }
 }
