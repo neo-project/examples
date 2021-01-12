502008c88d0e814f8e08314bb150ce3b68bfa4f8
陈志同 <chenzhitong12@qq.com>
2021-01-12 11:10:50 +0800

    Add Oracle project.

---
+++ csharp/Oracle/Oracle/Contract1.cs
@@ -0,0 +1,44 @@
+using Neo;
+using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using Neo.SmartContract.Framework.Services.System;
+using System;
+using System.ComponentModel;
+using System.Numerics;
+
+namespace Oracle
+{
+    [DisplayName("Oracle Demo")]
+    [ManifestExtra("Author", "Neo")]
+    [ManifestExtra("Email", "dev@neo.org")]
+    [ManifestExtra("Description", "This is an oracle example")]
+    public class OracleDemo : SmartContract
+    {
+        public static void DoRequest()
+        {
+            string url = "https://127.0.0.1:8080/test"; // the return value is  { "value": "hello world" }, and when we use private host for testing, don't forget to set `AllowPrivateHost` true
+            string filter = "$.value";  // JSONPath format https://github.com/atifaziz/JSONPath
+            string callback = "callback"; // callback method
+            object userdata = "userdata"; // arbitrary type
+            long gasForResponse = Oracle.MinimumResponseFee;
+
+            Oracle.Request(url, filter, callback, userdata, gasForResponse);
+        }
+
+        public static void Callback(string url, string userdata, int code, string result)
+        {
+            if (code != (int)OracleResponseCode.Success)
+            {
+                Runtime.Log("Oracle response failure with code " + Binary.Itoa(code));
+                return;
+            }
+
+            object ret = Json.Deserialize(result); // [ "hello world" ]
+            object[] arr = (object[])ret;
+            string value = (string)arr[0];
+
+            Runtime.Log("userdata: " + userdata);
+            Runtime.Log("response value: " + value);
+        }
+    }
+}
