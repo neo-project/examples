445384ef4a4c143d194387f1079ee809ddee39e5
陈志同 <chenzhitong12@qq.com>
2021-01-18 19:14:44 +0800

    Revert "Update all examples to 3.0.0-preview5"
    This reverts commit 227f30ed3794ff4b4dbb55959dbb77dd43f6a2e3.

---
+++ csharp/Lock/Lock.cs
@@ -0,0 +1,14 @@
+﻿using Neo.SmartContract.Framework.Services.Neo;
+
+namespace Neo.SmartContract
+{
+    public class Lock : Framework.SmartContract
+    {
+        public static bool Main(uint timestamp, byte[] pubkey, byte[] signature)
+        {
+            Header header = Blockchain.GetHeader(Blockchain.GetHeight());
+            if (timestamp > header.Timestamp) return false;
+            return VerifySignature(signature, pubkey);
+        }
+    }
+}
---
+++ csharp/StructExample/Point.cs
@@ -0,0 +1,8 @@
+﻿namespace Neo.SmartContract
+{
+    public struct Point
+    {
+        public int X;
+        public int Y;
+    }
+}
