4c0b92f6300a78c53a8dbda0a69c370c0970b293
陈志同 <chenzhitong12@qq.com>
2021-04-08 13:49:46 +0800

    Update OracleDemo.cs

--- csharp/Oracle/OracleDemo.cs
+++ csharp/Oracle/OracleDemo.cs
@@ -22,6 +22,7 @@ namespace Neo.SmartContract.Examples
 
         public static void Callback(string url, string userdata, OracleResponseCode code, string result)
         {
+            if (ExecutionEngine.CallingScriptHash != Oracle.Hash) throw new Exception("Unauthorized!");
             if (code != OracleResponseCode.Success) throw new Exception("Oracle response failure with code " + (byte)code);
 
             object ret = StdLib.JsonDeserialize(result); // [ "hello world" ]
